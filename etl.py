84a064a5aa37569f403dbb92adbdec0693a301fd
Jo Oh <joh@hubspot.com>
2022-07-13 01:12:35 -0400

    initial commit

---
+++ etl.py
@@ -0,0 +1,194 @@
+import configparser
+from datetime import datetime
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf, col, monotonically_increasing_id
+from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
+from load_tables import ProdQueries
+
+config = configparser.ConfigParser()
+config.read('dl.cfg')
+
+os.environ['AWS_ACCESS_KEY_ID']=config['AWS']['AWS_ACCESS_KEY_ID']
+os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS']['AWS_SECRET_ACCESS_KEY']
+
+
+def create_spark_session():
+    '''
+    Instantiate a SparkSession object using Hadoop-AWS package
+    Returns a SparkSession object
+    '''
+    spark = SparkSession \
+        .builder \
+        .appName("Immigration data exploration with Spark SQL") \
+        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.2") \
+        .getOrCreate()
+    return spark
+
+
+def process_immigration_data(spark, input_path, output_path, immigration_data_path, port_mapping_data_path, country_mapping_data_path):
+    '''
+    Load the song data stored in the input_data parameter into a Spark dataframe
+    Create songs and artists tables based on the song_data dataframe by writing them to parquet files
+    '''
+
+    ### fact_immigration table prep for prod load ###
+    ### data formatting achieved in the SQL input ###
+
+    # get filepath to immigration data files
+    # 1 subdirectory with hashed file names - need 1 wildcard for comprehensive search
+    immigration_data = os.path.join(input_path, immigration_data_path, '*.parquet')
+    
+    # read immigration data files
+    # define temp view to query
+    df_imm = spark.read.parquet(immigration_data)
+    df_imm.createOrReplaceTempView('stg_immigration')
+
+    # get filepath to port mapping for arrival city and state
+    port_mapping_data = os.path.join(input_path, port_mapping_data_path)
+
+    # read port mapping data file
+    # define temp view to query
+    df_port = spark.read.option('header', 'true').csv(port_mapping_data)
+    df_port.createOrReplaceTempView('port_mapping')
+
+    # extract and transform columns to create fact_immigration table
+    spark.sql(ProdQueries.fact_immigration_prod).createOrReplaceTempView('fact_immigration/')
+
+    # clean & format data further
+    # filter any rows that do not have valid arrival city or arrival date
+    fact_immigration_table = spark.sql('''
+    SELECT *
+    FROM fact_immigration
+    WHERE arrival_city is not null
+    AND arrival_date is not null
+    ''')
+    
+    # date format combined year and month column
+    fact_immigration_table = fact_immigration_table.withColumn('imm_report_month', to_date(col('imm_report_month', 'yyyy-mm-dd')))
+    
+    # write fact_immigration table to parquet files partitioned by arrival_date
+    fact_immigration_table.write.partitionBy('arrival_date') \
+                     .mode('overwrite') \
+                     .parquet(os.path.join(output_path, 'fact_immigration'))
+
+
+    ### dim_immigrant table prep for prod load ###
+    ### data formatting achieved in the SQL input ###
+
+    # get filepath to country mapping for citizenship and residence country
+    country_mapping_data = os.path.join(input_path, country_mapping_data_path)
+
+    # read country mapping data file
+    # define temp view to query
+    df_country = spark.read.option('header', 'true').csv(country_mapping_data)
+    df_country.createOrReplaceTempView('country_mapping')
+
+    # extract and transform columns to create dim_immigrant table
+    spark.sql(ProdQueries.dim_immigrant_prod).createOrReplaceTempView('dim_immigrant/')
+
+    # clean & format data further
+    # filter any rows that do not have valid gender or age
+    dim_immigrant_table = spark.sql('''
+    SELECT * 
+    FROM dim_immigrant 
+    WHERE gender in ('M', 'F')
+    AND age is not null
+    ''')
+
+    # write dim_immigrant table to a parquet file
+    dim_immigrant_table.write.mode('overwrite') \
+                             .parquet(os.path.join(output_path, 'dim_immigrant'))
+
+
+def process_temperature_data(spark, input_path, output_path, temperature_data_path):
+    '''
+    Load the log data stored in the input_data parameter into a Spark dataframe
+    Create users, time, and songplays tables based on the log_data dataframe by writing them to parquet files
+    '''
+    
+    # get filepath to temperature data file
+    temperature_data = os.path.join(input_path, temperature_data_path)
+
+    # read temperature data file
+    # filter by country = United States
+    # define temp view to query
+    df_temp = spark.read.option('header', 'true').csv(temperature_data)
+    df_temp = df_temp.where(df.country == 'United States')
+    df_temp.createOrReplaceTempView('stg_temperature')
+    
+    # extract and transform columns to create dim_temperature table
+    spark.sql(ProdQueries.dim_temperature_prod).createOrReplaceTempView('dim_temperature/')
+
+    # clean & format data further
+    # filter any rows before 1980 to only keep necessary data
+    # add a surrogate key for primary key
+    dim_temperature_table = spark.sql('''
+    SELECT *
+    FROM dim_temperature
+    WHERE temp_report_month >= date('1980-01-01')
+    ''').withColumn('temp_id', monotonically_increasing_id())
+    
+    # write dim_temperature table to a parquet file
+    dim_temperature_table.write.mode('overwrite') \
+                               .parquet(os.path.join(output_path, 'dim_temperature'))
+
+
+def process_demographics_data(spark, input_path, output_path, demographics_data_path):
+    '''
+    Load the log data stored in the input_data parameter into a Spark dataframe
+    Create users, time, and songplays tables based on the log_data dataframe by writing them to parquet files
+    '''
+    
+    # get filepath to demographics data file
+    demographics_data = os.path.join(input_path, demographics_data_path)
+
+    # read demographics data file
+    df_demo = spark.read.option('header', 'true').option('delimiter', ';').csv(demographics_data)
+
+    # format columns in snakecase without whitespace
+    # define temp view to query
+    df_demo = df_demo.select(['City', 'State Code', 'Race', 'Median Age', 'Male Population', 'Female Population', \
+        'Total Population', 'Number of Veterans', 'Foreign-born', 'Average Household Size'])
+    
+    new_demo_columns = ['city', 'state_code', 'race', 'median_age', 'male_population', 'female_population', \
+        'total_population', 'number_of_veterans', 'foreign_born', 'avg_household_size']
+    df_demo = rename_columns(df_demo, new_demo_columns)
+    df_demo.createOrReplaceTempView('stg_demographics') 
+
+    # extract and transform columns to create dim_demographics table
+    dim_demographics_table = spark.sql(ProdQueries.dim_demographics_prod)
+
+    # clean & format data further
+    # add a surrogate key for primary key
+    # unique by city || state_code || race
+    dim_demographics_table = dim_demographics_table.withColumn('demo_id', monotonically_increasing_id())
+    
+    # write dim_demographics table to a parquet file
+    dim_demographics_table.write.mode('overwrite') \
+                                .parquet(os.path.join(output_path, 'dim_demographics/'))
+
+
+def main():
+
+    # instantiate a spark session object
+    spark = create_spark_session()
+
+    # define input variables for S3 and data file path
+    input_path = config['S3']['STAGING_DATA']
+    output_path = config['S3']['PRODUCTION_DATA']
+
+    immigration_data_path = config['DATA']['IMMIGRATION']
+    port_mapping_data_path = config['DATA']['PORT_MAPPING']
+    country_mapping_data_path = config['DATA']['COUNTRY_MAPPING']
+    temperature_data_path = config['DATA']['TEMPERATURE']
+    demographics_data_path = config['DATA']['DEMOGRAPHICS']
+    
+    process_immigration_data(spark, input_path, output_path, immigration_data_path, port_mapping_data_path, country_mapping_data_path)    
+    process_temperature_data(spark, input_path, output_path, temperature_data_path) 
+    process_demographics_data(spark, input_path, output_path, demographics_data_path) 
+    
+    spark.stop()
+
+if __name__ == "__main__":
+    main()
---
+++ load_tables.py
@@ -0,0 +1,65 @@
+class ProdQueries:
+
+    fact_immigration_prod = '''
+    SELECT string(i.cicid) as immigration_id,
+    string(int(i.i94yr)) || '0' || string(int(i.i94mon)) || '01' as imm_report_month, -- need to be date formatted
+    case when i.i94mode = 1 then 'Air'
+         when i.i94mode = 2 then 'Sea'
+         when i.i94mode = 3 then 'Land'
+         when i.i94mode = 9 then 'Not reported'
+         else null end as transport_mode,
+    i.i94port as arrival_city_code,
+    p.city as arrival_city,
+    coalesce(i.i94addr, p.state) as arrival_state,
+    date_add(date('1960-01-01'), int(arrdate)) as arrival_date,
+    date_add(date('1960-01-01'), int(depdate)) as departure_date,
+    i.dtaddto as admitted_to_date, -- need to be date formatted, currently mmddyyyy
+    case when i.i94visa = 1 then 'Business'
+         when i.i94visa = 2 then 'Pleasure'
+         when i.i94visa = 3 then 'Student'
+         else null end as travel_purpose,
+    i.visatype as visa_type,
+    i.visapost as visa_issued_state,
+    i.airline as airline
+    FROM stg_immigration i
+    LEFT JOIN port_mapping p on lower(i.i94port) = lower(p.code)
+    '''
+
+    dim_immigrant_prod = '''
+    SELECT string(i.cicid) as immigration_id,
+    c1.country as citizenship_country,
+    c2.country as residence_country,
+    int(i.i94bir) as age,
+    int(i.biryear) as birthyear,
+    i.gender
+    FROM stg_immigration i
+    LEFT JOIN country_mapping c1 on lower(i.i94cit) = lower(c1.code)
+    LEFT JOIN country_mapping c2 on lower(i.i94res) = lower(c2.code)
+    '''
+
+    # need pk
+
+    dim_temperature_prod = '''
+    SELECT date(dt) as temp_report_month,
+    city,
+    country,
+    averagetemperature as avg_temp,
+    averagetemperatureuncertainty as avg_temp_uncertainty
+    FROM stg_temperature
+    '''
+
+    # need pk - city || state_code || race
+
+    dim_demographics_prod = '''
+    SELECT city,
+    state_code,
+    race,
+    median_age,
+    male_population,
+    female_population,
+    total_population,
+    number_of_veterans,
+    foreign_born,
+    avg_household_size
+    FROM stg_demographics
+    '''
